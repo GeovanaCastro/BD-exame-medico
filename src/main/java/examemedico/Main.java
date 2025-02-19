f4828e63c1093375fe94968613af9b82ae777cab
Geovana Castro <geovanacastro2003@gmail.com>
2025-02-19 09:08:31 -0300

    Material Coleta e Tipo Exame

--- src/main/java/examemedico/Main.java
+++ src/main/java/examemedico/Main.java
@@ -1,16 +1,10 @@
 package examemedico;
 
+import java.sql.SQLException;
+
 import examemedico.dao.DetalheExameResultadoDAO;
 import examemedico.dao.ExameResultadoDAO;
 import examemedico.dao.PessoaDAO;
-import examemedico.model.DetalheExameResultado;
-import examemedico.model.ExameResultado;
-import examemedico.model.Pessoa;
-
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
@@ -20,48 +14,51 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-//        Pessoa p1 = new Pessoa("alyne",12);
-//
-////        pessoaDAO.createPessoa(p1);
-//
-//        List<Pessoa> pessoas = pessoaDAO.getAllPessoas();
-//        pessoas.forEach(System.out::println);
+        // Pessoa p1 = new Pessoa("alyne",12);
+        //
+        //// pessoaDAO.createPessoa(p1);
+        //
+        // List<Pessoa> pessoas = pessoaDAO.getAllPessoas();
+        // pessoas.forEach(System.out::println);
 
-//        List<Pessoa> pessoasComIdade = pessoaDAO.getPessoasByIdade(25);
-//        for (Pessoa p : pessoasComIdade) {
-//            System.out.println(p);
-//        }
+        // List<Pessoa> pessoasComIdade = pessoaDAO.getPessoasByIdade(25);
+        // for (Pessoa p : pessoasComIdade) {
+        // System.out.println(p);
+        // }
 
-
-//        Pessoa pessoa1 = new Pessoa( "João Silva", 35);
-//        Pessoa pessoa2 = new Pessoa( "Maria Oliveira", 28);
-//
-//        pessoaDAO.createPessoa(pessoa1);
-//        pessoaDAO.createPessoa(pessoa2);
-//
-//        Timestamp dataColeta1 = Timestamp.valueOf("2025-02-01 10:30:00");
-//        Timestamp dataResultado1 = Timestamp.valueOf("2025-02-05 14:00:00");
-//
-//        Timestamp dataColeta2 = Timestamp.valueOf("2025-02-02 11:00:00");
-//        Timestamp dataResultado2 = Timestamp.valueOf("2025-02-06 15:30:00");
-//
-//        ExameResultado exame1 = new ExameResultado(dataColeta1, dataResultado1, 1, 2);
-//        ExameResultado exame2 = new ExameResultado(dataColeta2, dataResultado2, 2, 1);
-//
-//        exameResultadoDAO.createExameResultado(exame1);
-//        exameResultadoDAO.createExameResultado(exame2);
-//
-//        DetalheExameResultado detalhe1 = new DetalheExameResultado(1, "23", 9);
-//        DetalheExameResultado detalhe2 = new DetalheExameResultado(2, "45", 9);
-//
-//        DetalheExameResultadoDAO detalheResultadoDAO = new DetalheExameResultadoDAO();
-//        detalheResultadoDAO.createDetalheExameResultado(detalhe1);
-//        detalheResultadoDAO.createDetalheExameResultado(detalhe2);
-//
-//        List<DetalheExameResultado> detalhes = detalheResultadoDAO.findAllDetalheExameResultado();
-//
-//        for (DetalheExameResultado detalhe : detalhes) {
-//            System.out.println(detalhe);
-//        }
+        // Pessoa pessoa1 = new Pessoa( "João Silva", 35);
+        // Pessoa pessoa2 = new Pessoa( "Maria Oliveira", 28);
+        //
+        // pessoaDAO.createPessoa(pessoa1);
+        // pessoaDAO.createPessoa(pessoa2);
+        //
+        // Timestamp dataColeta1 = Timestamp.valueOf("2025-02-01 10:30:00");
+        // Timestamp dataResultado1 = Timestamp.valueOf("2025-02-05 14:00:00");
+        //
+        // Timestamp dataColeta2 = Timestamp.valueOf("2025-02-02 11:00:00");
+        // Timestamp dataResultado2 = Timestamp.valueOf("2025-02-06 15:30:00");
+        //
+        // ExameResultado exame1 = new ExameResultado(dataColeta1, dataResultado1, 1,
+        // 2);
+        // ExameResultado exame2 = new ExameResultado(dataColeta2, dataResultado2, 2,
+        // 1);
+        //
+        // exameResultadoDAO.createExameResultado(exame1);
+        // exameResultadoDAO.createExameResultado(exame2);
+        //
+        // DetalheExameResultado detalhe1 = new DetalheExameResultado(1, "23", 9);
+        // DetalheExameResultado detalhe2 = new DetalheExameResultado(2, "45", 9);
+        //
+        // DetalheExameResultadoDAO detalheResultadoDAO = new
+        // DetalheExameResultadoDAO();
+        // detalheResultadoDAO.createDetalheExameResultado(detalhe1);
+        // detalheResultadoDAO.createDetalheExameResultado(detalhe2);
+        //
+        // List<DetalheExameResultado> detalhes =
+        // detalheResultadoDAO.findAllDetalheExameResultado();
+        //
+        // for (DetalheExameResultado detalhe : detalhes) {
+        // System.out.println(detalhe);
+        // }
     }
 }
--- src/main/java/examemedico/dao/DetalheExameResultadoDAO.java
+++ src/main/java/examemedico/dao/DetalheExameResultadoDAO.java
@@ -1,18 +1,15 @@
 package examemedico.dao;
 
-import com.mysql.cj.x.protobuf.MysqlxCrud;
-import examemedico.connection.ConnectionModule;
-import examemedico.exception.DatabaseException;
-import examemedico.model.DetalheExameResultado;
-
-import java.sql.*;
-
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import examemedico.connection.ConnectionModule;
 import static examemedico.connection.ConnectionModule.prepareStatement;
+import examemedico.exception.DatabaseException;
+import examemedico.model.DetalheExameResultado;
 
 public class DetalheExameResultadoDAO {
 
@@ -29,7 +26,8 @@ public class DetalheExameResultadoDAO {
             preparedStatement.executeUpdate();
             System.out.println("Detalhe de resultado de exame criado com sucesso!");
         } catch (SQLException e) {
-            throw new DatabaseException("Erro ao criar detalhe de resultado de exame no banco de dados", e, e.getErrorCode(), e.getSQLState());
+            throw new DatabaseException("Erro ao criar detalhe de resultado de exame no banco de dados", e,
+                    e.getErrorCode(), e.getSQLState());
         }
     }
 
@@ -40,7 +38,8 @@ public class DetalheExameResultadoDAO {
             preparedStatement.executeUpdate();
             System.out.println("Detalhe de resultado de exame atualizado com sucesso!");
         } catch (SQLException e) {
-            throw new DatabaseException("Erro ao atualizar detalhe de resultado de exame no banco de dados", e, e.getErrorCode(), e.getSQLState());
+            throw new DatabaseException("Erro ao atualizar detalhe de resultado de exame no banco de dados", e,
+                    e.getErrorCode(), e.getSQLState());
         }
     }
 
@@ -50,13 +49,14 @@ public class DetalheExameResultadoDAO {
             preparedStatement.executeUpdate();
             System.out.println("Detalhe de resultado de exame deletado com sucesso!");
         } catch (SQLException e) {
-            throw new DatabaseException("Erro ao tentar deletar detalhe de resultado de exame do banco de dados", e, e.getErrorCode(), e.getSQLState());
+            throw new DatabaseException("Erro ao tentar deletar detalhe de resultado de exame do banco de dados", e,
+                    e.getErrorCode(), e.getSQLState());
         }
     }
 
     public DetalheExameResultado getByIdDetalheExameResultado(int id) throws SQLException {
         try (Connection connection = ConnectionModule.connection();
-             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_ID)) {
+                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_ID)) {
 
             preparedStatement.setInt(1, id);
 
@@ -66,8 +66,7 @@ public class DetalheExameResultadoDAO {
                     DetalheExameResultado detalhe = new DetalheExameResultado(
                             resultSet.getInt("id"),
                             resultSet.getString("valor_amostrado"),
-                            resultSet.getInt("exame_resultado_id")
-                    );
+                            resultSet.getInt("exame_resultado_id"));
 
                     System.out.println("Detalhe encontrado, Valor amostrado = " + detalhe.getValor_amostrado());
                     return detalhe;
@@ -77,23 +76,27 @@ public class DetalheExameResultadoDAO {
                 }
             }
         } catch (SQLException e) {
-            throw new DatabaseException("Erro ao procurar detalhe de resultado de exame no banco de dados", e, e.getErrorCode(), e.getSQLState());
+            throw new DatabaseException("Erro ao procurar detalhe de resultado de exame no banco de dados", e,
+                    e.getErrorCode(), e.getSQLState());
         }
     }
 
     public List<DetalheExameResultado> findAllDetalheExameResultado() throws SQLException {
         try (PreparedStatement preparedStatement = prepareStatement(SELECT);
-             ResultSet resultSet = preparedStatement.executeQuery()) {
+                ResultSet resultSet = preparedStatement.executeQuery()) {
 
             if (!resultSet.isBeforeFirst()) {
                 System.out.println("Nenhum registro encontrado na tabela detalhe_exame_resultado.");
             } else {
                 while (resultSet.next()) {
-                    System.out.println("ID: " + resultSet.getInt("id")+" | Valor Amostrado: "+ resultSet.getString("valor_amostrado")+" | Exame Resultado ID: "+ resultSet.getInt("exame_resultado_id"));
+                    System.out.println("ID: " + resultSet.getInt("id") + " | Valor Amostrado: "
+                            + resultSet.getString("valor_amostrado") + " | Exame Resultado ID: "
+                            + resultSet.getInt("exame_resultado_id"));
                 }
             }
         } catch (SQLException e) {
-            throw new DatabaseException("Erro ao buscar todos os detalhes de resultado de exame no banco de dados", e, e.getErrorCode(), e.getSQLState());
+            throw new DatabaseException("Erro ao buscar todos os detalhes de resultado de exame no banco de dados", e,
+                    e.getErrorCode(), e.getSQLState());
         }
         return List.of();
     }
